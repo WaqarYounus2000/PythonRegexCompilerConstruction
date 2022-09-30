2166b13c355a791436d078faf25a97b4046c3949
WaqarYounus2000 <83652319+WaqarYounus2000@users.noreply.github.com>
2022-09-30 17:48:33 +0500

    Update IDENTIFIERS (EB20102146).py

--- IDENTIFIERS (EB20102146).py
+++ IDENTIFIERS (EB20102146).py
@@ -1,21 +1,28 @@
 
 import re
 
-# pattern = '(int)\s(_|[a-z]*)[0-9]*'
+
+def IntegerFunction(test_string=""):
+    integerRegex = 'int\s+([a-z|A-Z|_][a-z|A-Z|0-9|_]*)\s*=\s*([^|-|+]([0-9]+))'
+    # test_string = 'int my_variable012 = -15478645.155'
+    result = re.fullmatch(integerRegex, test_string)
+    
+    if result:
+      print("Valid Identifiers.")
+      print(result)
+    else:
+      print("Invalid Identifiers")
+
+def FloatFunction(test_string=""):
+    floatRegex = 'float\s+([a-z|A-Z|_][a-z|A-Z|0-9|_]*)\s*=\s*([-|+|^]*)([0-9]*)\.([0-9]+)'  
+    result = re.fullmatch(floatRegex, test_string)
+    
+    if result:
+      print(result)
+      print("Valid Identifiers.")
+    else:
+      print("Invalid Identifiers")
 
 
-integerRegex = 'int\s+([a-z|A-Z|_][a-z|A-Z|0-9|_]*)\s*=\s*[-|+]([0-9]+)'
-
-test_string = 'int my_variable012 = -15478645.155'
-result = re.fullmatch(integerRegex, test_string)
-
-
-
-print("///////////////////////////////////////////////////////")
-print(result)
-if result:
-  print("Valid Identifiers.")
-  print("///////////////////////////////////////////////////////")
-else:
-  print("Invalid Identifiers")	
-  print("///////////////////////////////////////////////////////")
+# IntegerFunction("int my_variableIntg = 2154")
+FloatFunction("float _0my1_variable1float = 2.547")
