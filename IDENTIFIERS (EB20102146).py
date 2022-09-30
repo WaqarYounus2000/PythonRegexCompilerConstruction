9a16aac4aee7ed6ed5703d2c896ecad86d75d05c
WaqarYounus2000 <83652319+WaqarYounus2000@users.noreply.github.com>
2022-09-30 16:48:11 +0500

    updated

--- IDENTIFIERS (EB20102146).py
+++ IDENTIFIERS (EB20102146).py
@@ -1,11 +1,21 @@
 
 import re
 
-pattern = 'int=(_|[a-z]*)[0-9]*'
-test_string = 'int=waqar'
-result = re.match(pattern, test_string)
+# pattern = '(int)\s(_|[a-z]*)[0-9]*'
 
+
+integerRegex = 'int\s+([a-z|A-Z|_][a-z|A-Z|0-9|_]*)\s*=\s*[-|+]([0-9]+)'
+
+test_string = 'int my_variable012 = -15478645.155'
+result = re.fullmatch(integerRegex, test_string)
+
+
+
+print("///////////////////////////////////////////////////////")
+print(result)
 if result:
   print("Valid Identifiers.")
+  print("///////////////////////////////////////////////////////")
 else:
   print("Invalid Identifiers")	
+  print("///////////////////////////////////////////////////////")
